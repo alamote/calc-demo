79a729fc086850cfe3fccbd91962080cb81cb135
Sergey Lyskov <sergey.lyskov@onix-systems.com>
2021-11-23 00:19:43 +0200

    input listeners

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 const output = document.getElementById('output');
 const numbers = document.getElementsByClassName('number');
+const inlineActions = document.getElementsByClassName('inline-action');
+
 for (let i = 0; i < numbers.length; i++) {
 	numbers.item(i).addEventListener('click', event => {
+		if (output.value === '0') {
+			output.value = '';
+		}
 		output.value += event.target.innerHTML;
 	});
 }
+for (let i = 0; i < inlineActions.length; i++) {
+	inlineActions.item(i).addEventListener('click', event => {
+		if (/\d$/.test(output.value)) {
+			output.value += event.target.innerHTML;
+		}
+	});
+}
 document.getElementById('ce').addEventListener('click', () => {
-	output.value = '';
+	output.value = '0';
 });
-
+document.getElementById('period').addEventListener('click', () => {
+	if (output.value.indexOf('.') === -1) {
+		output.value += '.';
+	}
+});
+document.getElementById('equal').addEventListener('click', () => {
+	output.value = eval(output.value);
+});
 
 
 
