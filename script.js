d1f4cb89ef0fdfd78e048cbf8ee3290d7a91a742
Sergey Lyskov <sergey.lyskov@onix-systems.com>
2021-11-22 22:33:13 +0200

    init project

---
+++ script.js
@@ -0,0 +1,16 @@
+const output = document.getElementById('output');
+const numbers = document.getElementsByClassName('number');
+for (let i = 0; i < numbers.length; i++) {
+	numbers.item(i).addEventListener('click', event => {
+		output.value += event.target.innerHTML;
+	});
+}
+
+document.getElementById('ce').addEventListener('click', () => {
+	output.value = '';
+});
+
+
+
+
+
